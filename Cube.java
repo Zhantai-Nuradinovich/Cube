4ec25251ee7dd4158cd683dfcd7bf5394d50e87b
Zhantai Nuradinov <jantai.nuradinovich@gmail.com>
2019-12-09 12:12:47 +0300

    Класс кубиков 1

--- Cube.java
+++ Cube.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.awt.*;
 
 public class Cube {
+    private Facet[] facets;
+    public Cube(){
+        facets = new Facet[6];
+        //грань ХоУ
+        facets[0] = new Facet(new R3Vector(0,0,0), new R3Vector(1,0,0),
+                new R3Vector(1,1,0), new R3Vector(0,1,0));
+        //грань верхняя ХоЗ
+        facets[1] = new Facet(new R3Vector(0,1,0), new R3Vector(0,1,1),
+                new R3Vector(1,1,1), new R3Vector(1,1,0));
+        //грань верхняя УоЗ
+        facets[2] = new Facet(new R3Vector(1,0,0), new R3Vector(1,0,1),
+                new R3Vector(1,1,1), new R3Vector(1,1,0));
+        //грань верхняя ХоУ
+        facets[3] = new Facet(new R3Vector(0,0,1), new R3Vector(0,1,1),
+                new R3Vector(1,1,1), new R3Vector(1,0,1));
+        //грань нижняя ХоЗ
+        facets[4] = new Facet(new R3Vector(0,0,1), new R3Vector(0,0,0),
+                new R3Vector(1,0,0), new R3Vector(1,0,1));
+        //грань нижняя УоЗ
+        facets[5] = new Facet(new R3Vector(0,0,1), new R3Vector(0,0,0),
+                new R3Vector(0,1,1), new R3Vector(0,1,0));
+    }
+    public void rotate(double x,double y,double z){
+        for(int i = 0; i< facets.length; i++){
+            facets[i].travel(x,y,z);
+        }
+    }
+    public void travel(double x,double y,double z){
+        for(int i = 0; i< facets.length; i++){
+            facets[i].rotate(x,y,z);
+        }
+    }
+    public void draw(Graphics g){
+        
+    }
 }
