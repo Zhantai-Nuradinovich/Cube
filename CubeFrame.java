1b54f11392d124bc69901434abbe93ee74188e25
Zhantai Nuradinov <jantai.nuradinovich@gmail.com>
2019-12-09 12:22:58 +0300

    Окно куба

--- CubeFrame.java
+++ CubeFrame.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
-public class CubeFrame {
+public class CubeFrame extends JFrame {
+    private Cube cube;
+    public static int q = 500;
+    public CubeFrame(Cube cube){
+        this.cube = cube;
+        this.setSize(q,q);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setTitle("Я окно куба");
+    }
+    public void paint(Graphics g) {
+        
+    }
 }
