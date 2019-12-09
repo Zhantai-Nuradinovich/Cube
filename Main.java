3167aba9761c1e3dcd00f2d7a34206d289e04f1c
Zhantai Nuradinov <jantai.nuradinovich@gmail.com>
2019-12-09 10:57:32 +0300

    Вызов тут

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+        /*R3Vector a = new R3Vector(10, 0,0);
+        a.rotateOY(90);
+        a.rotateOY(90);
+        a.rotateOY(90);
+        a.rotateOY(90);*/
+        R3Vector a = new R3Vector(0,0,0);
+        R3Vector b = new R3Vector(1,0,0);
+        R3Vector c = new R3Vector(1,1,0);
+        R3Vector d = new R3Vector(0,1,0);
+        //System.out.println(a.x+" "+a.y+" "+a.z );
+        Facet face = new Facet(a,b,c,d);
+        face.print();
+        face.rotate(90,0,0);
+        face.print();
+    }
+}
+//
