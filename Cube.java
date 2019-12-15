aa264b714f12766fb3fa00a77d415dc16ea8b044
Zhantai Nuradinov <jantai.nuradinovich@gmail.com>
2019-12-16 00:15:39 +0300

    Класс куба версия2

--- Cube.java
+++ Cube.java
@@ -4,36 +4,55 @@ public class Cube {
     private Facet[] facets;
     public Cube(){
         facets = new Facet[6];
-        //грань ХоУ
-        facets[0] = new Facet(new R3Vector(0,0,0), new R3Vector(1,0,0),
-                new R3Vector(1,1,0), new R3Vector(0,1,0));
+        //нижняя грань ХоУ
+        //facets[0] = new Facet(new R3Vector(0,0,0), new R3Vector(1,0,0), // те, что сверху являются гранями против часовой
+                //new R3Vector(1,1,0), new R3Vector(0,1,0));
+        facets[0] = new Facet(new R3Vector(0,0,0), new R3Vector(0,1,0),
+                new R3Vector(1,1,0), new R3Vector(1,0,0));
         //грань верхняя ХоЗ
-        facets[1] = new Facet(new R3Vector(0,1,0), new R3Vector(0,1,1),
-                new R3Vector(1,1,1), new R3Vector(1,1,0));
+        //facets[1] = new Facet(new R3Vector(0,1,0), new R3Vector(0,1,1),
+            //    new R3Vector(1,1,1), new R3Vector(1,1,0));
+        facets[1] = new Facet(new R3Vector(1,1,0), new R3Vector(0,1,0),
+                new R3Vector(0,1,1), new R3Vector(1,1,1));
         //грань верхняя УоЗ
-        facets[2] = new Facet(new R3Vector(1,0,0), new R3Vector(1,0,1),
-                new R3Vector(1,1,1), new R3Vector(1,1,0));
-        //грань верхняя ХоУ
-        facets[3] = new Facet(new R3Vector(0,0,1), new R3Vector(0,1,1),
+        //facets[2] = new Facet(new R3Vector(1,0,0), new R3Vector(1,0,1),
+              //  new R3Vector(1,1,1), new R3Vector(1,1,0));
+        facets[2] = new Facet(new R3Vector(1,0,0), new R3Vector(1,1,0),
                 new R3Vector(1,1,1), new R3Vector(1,0,1));
+        //грань верхняя ХоУ
+        //facets[3] = new Facet(new R3Vector(0,0,1), new R3Vector(0,1,1),
+              //  new R3Vector(1,1,1), new R3Vector(1,0,1));
+        facets[3] = new Facet(new R3Vector(0,0,1), new R3Vector(1,0,1),
+                new R3Vector(1,1,1), new R3Vector(0,1,1));
         //грань нижняя ХоЗ
-        facets[4] = new Facet(new R3Vector(0,0,1), new R3Vector(0,0,0),
-                new R3Vector(1,0,0), new R3Vector(1,0,1));
+        //facets[4] = new Facet(new R3Vector(0,0,1), new R3Vector(0,0,0),
+               // new R3Vector(1,0,0), new R3Vector(1,0,1));
+        facets[4] = new Facet(new R3Vector(0,0,0), new R3Vector(1,0,0),
+                new R3Vector(1,0,1), new R3Vector(0,0,1));
         //грань нижняя УоЗ
-        facets[5] = new Facet(new R3Vector(0,0,1), new R3Vector(0,0,0),
-                new R3Vector(0,1,1), new R3Vector(0,1,0));
+        //facets[5] = new Facet(new R3Vector(0,0,1), new R3Vector(0,0,0),
+               // new R3Vector(0,1,1), new R3Vector(0,1,0));
+        facets[5] = new Facet(new R3Vector(0,1,0), new R3Vector(0,0,0),
+                new R3Vector(0,0,1), new R3Vector(0,1,1));
     }
     public void rotate(double x,double y,double z){
-        for(int i = 0; i< facets.length; i++){
-            facets[i].travel(x,y,z);
-        }
-    }
-    public void travel(double x,double y,double z){
         for(int i = 0; i< facets.length; i++){
             facets[i].rotate(x,y,z);
         }
     }
+    public void travel(double x,double y,double z){
+        for(int i = 0; i< facets.length; i++){
+            facets[i].travel(x,y,z);
+        }
+    }
     public void draw(Graphics g){
-        
+        for(int i = 0; i< facets.length; i++){
+            facets[i].draw(g);
+        }
+    }
+    public void scale(double k) {
+        for(int i = 0; i< facets.length; i++){
+            facets[i].scale(k);
+        }
     }
 }
